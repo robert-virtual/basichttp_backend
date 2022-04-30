6a362e9c5bd249940995bdb3301ca8921c717e9c
robert-virtual <robertocastill63@gmail.com>
2022-04-29 18:14:51 -0600

    port

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { __prod__ } = require("./const");
 const app = express();
-const port = 3000;
+const port = 3000 || process.env.PORT;
 
 if (!__prod__) {
   console.log("development");
